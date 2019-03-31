ae99b07b0a79948de1aae2d8ea315fcb93d772d0
Simon Sørensen <6843486+simse@users.noreply.github.com>
2019-03-31 19:13:05 +0200

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,32 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Footer from '../components/footer'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Yikes." />
+
+    <div style={{
+      textAlign: 'center',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <h1 style={{
+        fontSize: '5rem',
+        fontWeight: '300'
+      }}>404</h1>
+
+      <p style={{
+        textTranform: 'uppercase',
+        fontSize: '1.3rem'
+      }}>Yikes, that page doesn't exist.</p>
+    </div>
+
+    <Footer />
   </Layout>
 )
 
