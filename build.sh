244cfc6f8e9c866231205f14fd39ea3c3cbc29ba
Simon Sørensen <hello@simse.io>
2019-10-24 21:55:44 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ find . -name '*.html' -type f -exec sed -i 's/jpegg/jpeg/g' {} +
 find . -name '*.html' -type f -exec sed -i 's/jpegeg/jpeg/g' {} +
 find . -name '*.html' -type f -exec sed -i 's/jpegpeg/jpeg/g' {} +
 find . -name '*.html' -type f -exec sed -i 's/jpegjpeg/jpeg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/pngg/png/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/pngng/png/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/pngpng/png/g' {} +
 cp _redirects publish/_redirects
