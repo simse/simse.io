9320efd28af05e27973b421e41204953c9908b80
Simon Sørensen <hello@simse.io>
2019-10-24 19:37:53 +0100

    Mirror of Ghost site

---
+++ build.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+pip install buster
+buster generate --domain https://ghost.simse.io --dir publish/
+find . -name '*.html' -type f -exec sed -i 's/jpgg/jpg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/jpgpg/jpg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/jpgjpg/jpg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/jpegg/jpeg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/jpegeg/jpeg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/jpegpeg/jpeg/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/jpegjpeg/jpeg/g' {} +
+cp _redirects publish/_redirects
