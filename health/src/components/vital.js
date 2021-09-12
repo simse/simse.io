7acb529219ca58540738444d85bcdc2e4b749c29
Simon Sørensen <hello@simse.io>
2021-09-12 23:29:03 +0200

    added rudimentary health page

---
+++ health/src/components/vital.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import * as styles from "../styles/components/vital.module.scss"
+
+// markup
+const Vital = (props) => {
+  return (
+    <div className={styles.vital}>
+        <strong className={styles.label}>{props.label}</strong>
+
+        <div className={styles.valueContainer}>
+            <h2 className={styles.value}>{props.value}</h2>
+            <span className={styles.unit}>{props.unit}</span>
+        </div>
+    </div>
+  )
+}
+
+export default Vital
