4a2c12df626d216deb4bba239bb1fb3c590e6e71
Simon Sørensen <hello@simse.io>
2021-09-12 22:35:02 +0200

    add health subsite

---
+++ health/gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Simon's Health",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
