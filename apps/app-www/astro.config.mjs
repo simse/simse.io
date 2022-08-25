6a792801f4e12efd45cf0a6e0e578b9b25fbe3d3
Simon Sorensen <hello@simse.io>
2022-08-25 23:58:26 +0100

    feat: Add robots.txt

--- apps/app-www/astro.config.mjs
+++ apps/app-www/astro.config.mjs
@@ -3,12 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), compress(), image()],
+  site: 'https://simse.io',
+  integrations: [tailwind(), svelte(), mdx(), compress(), image(), sitemap()],
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     syntaxHighlight: 'prism'
---
+++ apps/app-www/content/notes/github-username.md
@@ -0,0 +1,5 @@
+---
+title: The Story of How I Got My Github Username
+slug: how-i-got-my-github-username
+---
+This is an interesting one
