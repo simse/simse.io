440dfe0493efe9ed3215931e7c22b7bb5dd4a1b5
Simon Sorensen <hello@simse.io>
2023-04-02 22:52:38 +0100

    feat: Update UI to use new service-chat

--- apps/app-www/src/components/AI.tsx
+++ apps/app-www/src/components/AI.tsx
@@ -1,56 +1,37 @@
 import { useState, useEffect, useRef } from "react";
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+import type { ClientMessage, ServerMessage, Message, ContentCard, Conversation } from '@simse/service-chat/src/types';
 
-interface Message {
-    id: string;
-    message: string;
-    entity: "user" | "model" | "system" | "overlord";
-    cards?: Card[];
-    finished: boolean;
-}
-
-interface Card {
-    title: string;
-    body: string;
-    url?: string;
-    image?: string;
-}
-
-interface Conversation {
+interface ConversationWithMessages extends Conversation {
     messages: Message[];
-    model: string;
 }
 
 interface UserMessageProps {
     message: Message;
 }
 
-// regex for model URL output: (DISPLAY|GET) (\/[a-zA-Z0-9&?=]*)
-// regex for partially formed URL output: (D|DI|DIS|DISP|DISPLA|DISPLAY)|(G|GE|GET)
-
-
 const UserMessage = ({ message }: UserMessageProps) => (
     <div className="flex gap-2 mb-3 text-zinc-400">
-        {message.message}
+        {message.text}
     </div>
 )
 
 interface ModelMessageProps {
-    message: Message;
+    message: Message & { cards?: ContentCard[] }
 }
 
 const ModelMessage = ({ message }: ModelMessageProps) => (
     <div className="mb-6 text-xl">
-        {message.message}
+        {message.text}
 
-        {message.finished && message.cards?.map(card => <Card key={card.title} card={card} />)}
+        {/*message.finished && message.cards?.map(card => <Card key={card.title} card={card} />)*/}
 
-        {(!message.finished && message.cards) && <LoadingCard />}
+        {(true && message.cards) && <LoadingCard />}
     </div>
 )
 
 interface CardProps {
-    card: Card;
+    card: ContentCard;
 }
 
 const Card = ({ card }: CardProps) => (
@@ -66,102 +47,51 @@ const LoadingCard = () => (
     </div>
 )
 
+enum ConnectionStatus {
+    CONNECTING = "Connecting...",
+    CONNECTED = "Connected",
+    DISCONNECTED = "Disconnected"
+}
+
 const AI = () => {
     const messagesContainer = useRef<HTMLInputElement>(null);
     const [query, setQuery] = useState<string>("");
-    const [conversation, setConversation] = useState<Conversation>({
-        messages: [
-            /*{
-                id: "1",
-                message: "show me your latest blog posts",
-                entity: "user",
-                finished: true
-            },
-            {
-                id: "2",
-                message: `Sure here it is:`,
-                entity: "model",
-                finished: false,
-                cards: [
-                    {
-                        title: "GPT-4 is awesome!",
-                        body: "GPT-3.5 felt like a huge step forward, GPT-4 takes it to the next level",
-                    },
-                ]
-            },
-            {
-                id: "1",
-                message: "show me your latest blog posts",
-                entity: "user",
-                finished: true
-            },
-            {
-                id: "2",
-                message: `Sure here it is:`,
-                entity: "model",
-                finished: false,
-                cards: [
-                    {
-                        title: "GPT-4 is awesome!",
-                        body: "GPT-3.5 felt like a huge step forward, GPT-4 takes it to the next level",
-                    },
-                ]
-            },
-            {
-                id: "1",
-                message: "show me your latest blog posts",
-                entity: "user",
-                finished: true
-            },
-            {
-                id: "2",
-                message: `Sure here it is:`,
-                entity: "model",
-                finished: false,
-                cards: [
-                    {
-                        title: "GPT-4 is awesome!",
-                        body: "GPT-3.5 felt like a huge step forward, GPT-4 takes it to the next level",
-                    },
-                ]
-            },*/
-        ],
-        model: "gpt-4"
-    });
+    const [conversation, setConversation] = useState<Conversation>();
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [status, setStatus] = useState<ConnectionStatus>(ConnectionStatus.CONNECTING);
 
-    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket('ws://localhost:8080/chat', {}, true);
+    const { sendMessage, lastJsonMessage, readyState } = useWebSocket(import.meta.env.PUBLIC_SERVICE_CHAT_URL || '', {}, true);
 
     useEffect(() => {
         if (lastJsonMessage !== null) {
-            const message = lastJsonMessage as unknown as Message;
-            if (message.entity === 'overlord') return;
+            const serverMessage = lastJsonMessage as unknown as ServerMessage;
 
-            // check if message with id already exists, if so replace it
-            const existingMessage = conversation.messages.find(m => m.id === message.id);
-            if (existingMessage) {
-                const messages = conversation.messages.map(m => {
-                    if (m.id === message.id) {
-                        return message;
+            //console.log(serverMessage);
+            
+            switch(serverMessage.type) {
+                case "CONVERSATION_CREATED":
+                    setConversation(serverMessage.conversation);
+                    setStatus(ConnectionStatus.CONNECTED);
+                    break;
+                    
+                case "MESSAGE_SENT":
+                    const existingMessage = messages.find(m => m.id === serverMessage.message?.id);
+
+                    if (existingMessage) {
+                        setMessages(messages.map(m => {
+                            if (m.id === serverMessage.message?.id) {
+                                return serverMessage.message;
+                            }
+
+                            return m;
+                        }));
+                    } else {
+                        setMessages([
+                            ...messages,
+                            serverMessage.message as Message
+                        ]);
                     }
-
-                    return m;
-                });
-
-                setConversation({
-                    ...conversation,
-                    messages
-                });
-            }
-
-            // otherwise add it to the conversation
-            else {
-                setConversation({
-                    ...conversation,
-                    messages: [
-                        ...conversation.messages,
-                        message
-                    ]
-                });
+                    break;
             }
         }
       }, [lastJsonMessage, setConversation]);
@@ -173,50 +103,59 @@ const AI = () => {
         })
     }, [conversation]);
 
+    useEffect(() => {
+        if (readyState === ReadyState.CLOSED) {
+            setStatus(ConnectionStatus.DISCONNECTED);
+        } else {
+            setStatus(ConnectionStatus.CONNECTING);
+        }
+    }, [readyState]);
+
     const submitMessage = () => {
         if (query === "") {
             return;
         }
 
-        setConversation({
-            ...conversation,
-            messages: [
-                ...conversation.messages,
-                {
-                    id: (conversation.messages.length + 1).toString(),
-                    message: query,
-                    entity: "user",
-                    finished: true
-                }
-            ]
-        });
+        setMessages([
+            ...messages,
+            {
+                conversationId: conversation?.id || "ddd",
+                id: (Math.random() * 100000000000000000).toString(),
+                text: query,
+                entity: "USER",
+                timestamp: new Date()
+            }
+        ]);
 
-        sendJsonMessage({
-            id: (conversation.messages.length + 1).toString(),
+        const message: ClientMessage = {
+            conversationId: conversation?.id || "ddd",
             message: query,
-        });
+            type: "SEND_MESSAGE"
+        }
+
+        sendMessage(JSON.stringify(message));
 
         setQuery("");
     }
 
     const connectionMessage = (): string => {
-        if (readyState === ReadyState.CONNECTING) {
+        if (status === ConnectionStatus.CONNECTING) {
             return "Connecting to GPT-4...";
         }
 
-        if (readyState === ReadyState.OPEN) {
-            return "Connected to GPT-3.5";
+        if (status === ConnectionStatus.CONNECTED) {
+            return "Connected to GPT-4";
         }
 
         return "Disconnected";
     }
 
     const connectionColour = (): string => {
-        if (readyState === ReadyState.CONNECTING) {
+        if (status === ConnectionStatus.CONNECTING) {
             return "bg-yellow-600";
         }
 
-        if (readyState === ReadyState.OPEN) {
+        if (status === ConnectionStatus.CONNECTED) {
             return "bg-green-600";
         }
 
@@ -226,27 +165,27 @@ const AI = () => {
     return (
         <div>
             <div ref={messagesContainer} className="pr-2 max-h-[30vh] overflow-y-auto scrollbar-thumb-neutral-800 scrollbar-track-neutral-900 scrollbar-thin scrollbar-rounded">
-                {conversation.messages.map((message, i) => {
-                    if (message.entity === "user") {
+                {messages.map((message, i) => {
+                    if (message.entity === "USER") {
                         return <UserMessage key={message.id} message={message} />
                     }
 
-                    if (message.entity === "model") {
+                    if (message.entity === "MODEL") {
                         return <ModelMessage key={message.id} message={message} />
                     }
                 })}
             </div>
 
-            <div className={`flex gap-1 w-full ${conversation.messages.length > 0 ? 'mt-12' : ''}`}>
-                <input 
-                    className="bg-zinc-900 py-3 px-4 text-xl w-5/6 active:outline outline-2 outline-blue-600 border-0" 
-                    type="text" 
-                    placeholder={conversation.messages.length === 0 ? "What are you looking for?" : "Type your message..."}
+            <div className={`flex gap-1 w-full ${messages.length > 0 ? 'mt-12' : ''}`}>
+                <input
+                    className="bg-zinc-900 py-3 px-4 text-xl w-5/6 active:outline outline-2 outline-blue-600 border-0"
+                    type="text"
+                    placeholder={messages.length === 0 ? "What are you looking for?" : "Type your message..."}
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
-                    onKeyDown={(e) => e.code === "Enter" ? submitMessage() : null}/>
+                    onKeyDown={(e) => e.code === "Enter" ? submitMessage() : null} />
 
-                <button 
+                <button
                     className="bg-blue-600 w-1/6 disabled:bg-zinc-700 disabled:hover:cursor-not-allowed"
                     disabled={readyState !== ReadyState.OPEN}
                     onClick={submitMessage}>Send</button>
---
+++ services/service-chat/src/types.ts
@@ -0,0 +1,10 @@
+import type { Message, ContentCard, Conversation } from "@prisma/client";
+import type { ServerMessage, ClientMessage } from "./runtime/runtime";
+
+export type {
+    Message,
+    ContentCard,
+    Conversation,
+    ServerMessage,
+    ClientMessage
+}
