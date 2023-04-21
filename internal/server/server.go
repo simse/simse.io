249077730e4fea7b72e596de6b07bccab92be69c
Simon Sorensen <hello@simse.io>
2023-04-21 17:23:13 +0100

    feat: Add subdomain routing

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gofiber/adaptor/v2"
@@ -18,6 +19,12 @@ type ServerInfo struct {
 	Region string
 }
 
+type (
+	Host struct {
+		Fiber *fiber.App
+	}
+)
+
 func StartServer() {
 	// load templates
 	engine := jet.NewFileSystem(http.FS(templates.Files), ".jet")
@@ -26,12 +33,49 @@ func StartServer() {
 		return date.Format("January 2, 2006")
 	})
 
-	app := fiber.New(fiber.Config{
-		Views:                 engine,
-		DisableStartupMessage: true,
-		ServerHeader:          "Microsoft-IIS/7.5",
+	hosts := map[string]*Host{}
+
+	rootApp := fiber.New(fiber.Config{
+		Views:        engine,
+		ServerHeader: "Microsoft-IIS/7.5",
 	})
 
+	// routes
+	rootApp.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/index", fiber.Map{
+			"pageTitle": "Simon Sorensen — Classically Trained Software Engineer",
+		}, "layouts/content")
+	})
+
+	rootApp.Get("/sitemap", func(c *fiber.Ctx) error {
+		return c.Render("pages/sitemap", fiber.Map{
+			"pageTitle": "Sitemap — Simon Sorensen",
+		}, "layouts/container")
+	})
+
+	rootApp.Get("/projects", func(c *fiber.Ctx) error {
+		return c.Render("pages/projects", fiber.Map{
+			"pageTitle": "My Projects — Simon Sorensen",
+		}, "layouts/container")
+	})
+
+	rootApp.Get("/posts", PostsIndex)
+	rootApp.Get("/posts/:slug", PostRoute)
+
+	rootApp.Static("/static", "./static", fiber.Static{
+		Compress:      true,
+		CacheDuration: 30 * 24 * time.Hour,
+	})
+	rootApp.Get("/static/live.js", adaptor.HTTPHandler(live.Javascript{}))
+	rootApp.Get("/static/auto.js.map", adaptor.HTTPHandler(live.JavascriptMap{}))
+	rootApp.Get("/_image", imageHandler)
+
+	hosts[os.Getenv("SIMSE_IO_HOST")] = &Host{Fiber: rootApp}
+
+	log.Info().Str("address", "0.0.0.0").Int("port", 3000).Msg("server started")
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
 	app.Use(requestid.New())
 
 	// enable logging
@@ -44,30 +88,14 @@ func StartServer() {
 
 	// enable compression
 	app.Use(compress.New())
-
-	// routes
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("pages/index", fiber.Map{
-			"pageTitle": "Simon Sorensen — Classically Trained Software Engineer",
-		}, "layouts/content")
+	app.Use(func(c *fiber.Ctx) error {
+		host := hosts[c.Hostname()]
+		if host == nil {
+			return c.SendStatus(fiber.StatusNotFound)
+		} else {
+			host.Fiber.Handler()(c.Context())
+			return nil
+		}
 	})
-
-	app.Get("/sitemap", func(c *fiber.Ctx) error {
-		return c.Render("pages/sitemap", fiber.Map{
-			"pageTitle": "Sitemap — Simon Sorensen",
-		}, "layouts/container")
-	})
-
-	app.Get("/posts", PostsIndex)
-	app.Get("/posts/:slug", PostRoute)
-
-	app.Static("/static", "./static", fiber.Static{
-		Compress: true,
-	})
-	app.Get("/static/live.js", adaptor.HTTPHandler(live.Javascript{}))
-	app.Get("/static/auto.js.map", adaptor.HTTPHandler(live.JavascriptMap{}))
-	app.Get("/_image", imageHandler)
-
-	log.Info().Str("address", "0.0.0.0").Int("port", 3000).Msg("server started")
 	app.Listen(":3000")
 }
