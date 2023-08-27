a0a4067154f56f38521168ae28ff2f44faed0c35
Simon Sorensen <hello@simse.io>
2023-08-27 14:55:06 +0100

    feat: Fix font face definition

--- src/components/homepage/writing/Slider.svelte
+++ src/components/homepage/writing/Slider.svelte
@@ -1,23 +1,10 @@
 <style lang="scss">
     .image {
-        opacity: 0;
         transition-duration: 800ms;
-
-        &.selected {
-            opacity: 1;
-        }
     }
 
     .aspect-square {
         aspect-ratio: 1 / 1;
-    }
-
-    .text {
-        opacity: 0;
-
-        &.selected {
-            opacity: 1;
-        }
     }
 </style>
 
@@ -51,7 +38,7 @@
     <!-- images -->
     <div class="h-[90vw] w-[90vw] lg:h-[50vw] lg:w-[50vw] max-w-[800px] max-h-[800px] grid grid-cols-1">
         {#each slides as slide, index}
-        <div class={`transition-opacity image ${currentSlide === index ? 'selected' : ''} col-start-1 row-start-1`}>
+        <div class={`transition-opacity image ${currentSlide === index ? 'opacity-1' : 'opacity-0'} col-start-1 row-start-1`}>
             <img src={slide.image.src} width={800} height={800} alt={slide.imageAlt} class="object-cover" />
 
             <p class="-mt-3 lg:-mt-5 font-[NewYork] text-[#BA855E] text-4xl select-none uppercase">{slide.imageTitle}</p>
@@ -77,7 +64,13 @@
     <button class="mr-auto border border-[#BA855E] text-[#BA855E] hover:bg-[#BA855E] hover:text-black transition-all px-4 py-2 rounded-full" on:click={previousSlide}>← Previous Fact</button>
 
     {#each slides as _, index}
-    <div class={`h-3 w-3 rounded-full border border-[#BA855E] ${currentSlide === index ? 'bg-[#BA855E]' : ''}`}></div>
+    <div 
+        class={`h-3 w-3 rounded-full border border-[#BA855E] ${currentSlide === index ? 'bg-[#BA855E]' : ''} hover:cursor-pointer`}
+        role="button" 
+        tabindex="0" 
+        on:click={() => {currentSlide = index}}
+        on:keypress={() => {currentSlide = index}}
+    />
     {/each}
 
     <button class="ml-auto border border-[#BA855E] text-[#BA855E] hover:bg-[#BA855E] hover:text-black transition-all px-4 py-2 rounded-full" on:click={nextSlide}>Next Fact →</button>
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,14 @@
 interface PageMeta {
     title: string;
     description: string;
-
+    fonts?: Font[];
 }
 
-
+interface Font {
+    name: string;
+    url: string;
+    weight: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+}
 
 export type {
     PageMeta
