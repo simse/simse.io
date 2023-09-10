ef6a94ade266c7bcec1bfcc5740ae3842d69df4d
Simon Sorensen <hello@simse.io>
2023-09-10 19:14:38 +0100

    feat: Update frontpage hero

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,9 @@
 import slugify from "slugify";
 
 export const slugifyText = (input: string) => slugify(input, { lower: true });
+
+export const formatPostDate = (date: Date): string => date.toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+});
