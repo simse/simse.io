c05bc5b2ad746e9a0d01077b4f57926ee7404e51
Simon Sorensen <hello@simse.io>
2024-08-01 22:08:50 +0100

    feat: Add support for pypi download stats (#65)

--- src/pages/api/download-stats/[slug].json.ts
+++ src/pages/api/download-stats/[slug].json.ts
@@ -1,6 +1,9 @@
 import type { APIRoute } from "astro";
+import TTLCache from '@isaacs/ttlcache';
 
 export const prerender = false;
+
+const cache = new TTLCache({ max: 10000, ttl: 60 * 60 * 1000 });
 
 export const GET: APIRoute = async ({ params, request }) => {
   const slug = params.slug;
@@ -16,25 +19,49 @@ export const GET: APIRoute = async ({ params, request }) => {
     );
   }
 
-  if (slug === "chronos") {
-    const downloads = await getDockerhubDownloads("simsemand/chronos");
+  const downloads = await getDownloadsCached(slug);
 
+  if (!downloads) {
     return new Response(
-      JSON.stringify({
-        downloads: downloads,
-      }),
+        JSON.stringify({
+          error: `Could not fetch statistics for project with slug: ${slug}`,
+        }),
+        {
+          status: 404,
+        },
     );
   }
 
   return new Response(
-    JSON.stringify({
-      error: `Could not fetch statistics for project with slug: ${slug}`,
-    }),
-    {
-      status: 404,
-    },
+      JSON.stringify({
+        downloads: downloads,
+      }),
   );
 };
+
+const getDownloadsCached = async (identifier: string) => {
+  if (cache.has(identifier)) {
+    return cache.get<number>(identifier);
+  }
+
+  const downloads = await getDownloads(identifier);
+  if (downloads && downloads !== 0) {
+    cache.set(identifier, downloads);
+  }
+
+  return downloads;
+}
+
+const getDownloads = async (identifier: string) => {
+  if (identifier === "chronos") {
+    return getDockerhubDownloads("simsemand/chronos");
+  }
+  if (identifier === 'pymitv') {
+    return getPypiDownloads('pymitv');
+  }
+
+  return undefined;
+}
 
 const getDockerhubDownloads = async (identifier: string): Promise<number> => {
   const response = await fetch(
@@ -48,3 +75,21 @@ const getDockerhubDownloads = async (identifier: string): Promise<number> => {
   const parsedResponse = await response.json();
   return parsedResponse.pull_count as number;
 };
+
+const getPypiDownloads = async (identifier: string): Promise<number> => {
+  const response = await fetch(
+      `https://api.pepy.tech/api/v2/projects/${identifier}`,
+      {
+        headers: {
+          'x-api-key': import.meta.env.PEPY_API_KEY
+        }
+      }
+  );
+
+  if (!response.ok) {
+    return 0;
+  }
+
+  const parsedResponse = await response.json();
+  return parsedResponse.total_downloads as number;
+}
