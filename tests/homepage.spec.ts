1952e234145244fd1d636c931ecc5ff838c9c1b0
simse <hello@simse.io>
2024-08-16 22:39:04 +0100

    feat: Add homepage tests

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -16,3 +16,26 @@ test('contains header element', async ({ page }) => {
   // Ensure header contains text
   await expect(page.getByRole('banner')).toContainText('Hej');
 });
+
+test('contains blog posts section', async ({ page }) => {
+  await page.goto('/');
+
+  const blogSection = page.getByTitle('Blog');
+
+  await expect(blogSection).toBeVisible();
+
+  const blogPosts = blogSection.locator('ul > li');
+  expect(await blogPosts.count()).toBeGreaterThan(0);
+  expect(await blogPosts.count()).toBeLessThanOrEqual(4);
+});
+
+test('contains projects section', async ({ page }) => {
+  await page.goto('/');
+
+  const projectsSection = page.getByTitle('Projects');
+
+  await expect(projectsSection).toBeVisible();
+
+  const projects = projectsSection.locator('ul > li');
+  expect(await projects.count()).toBeGreaterThan(0);
+});
