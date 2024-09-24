91948a3d4a2d4d8dc36bf083addd9f15ba58fea7
simse <hello@simse.io>
2024-09-24 15:47:38 +0100

    feat: Add button unit test

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { configDefaults } from 'vitest/config'
 import { getViteConfig } from 'astro/config'
 
 export default getViteConfig({
-  // @ts-ignore FIX LATER
   test: {
     exclude:[
       ...configDefaults.exclude,
