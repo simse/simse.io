af34f0ee8fdad2b0ffaea945b61d74b7d3a550f1
Simon Sorensen <hello@simse.io>
2024-09-26 20:29:54 +0100

    fix: Revert from Bun to pnpm (#90)
    * fix: Revert switch to Bun package manager :(
    
    * ci: Fix issue with workflow
    
    * fix: Fix yaml error
    
    * fix: Fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,8 +71,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-     command: 'bun run build && bun run start',
-     url: 'http://127.0.0.1:4321',
-     reuseExistingServer: !process.env.CI,
+    command: 'pnpm build && pnpm start',
+    url: 'http://127.0.0.1:4321',
+    reuseExistingServer: !process.env.CI,
   },
 });
